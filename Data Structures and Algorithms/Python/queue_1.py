25d02d23e671e56ab777dc575f6887ec4622048e
Akorede <akoredeadewole8@gmail.com>
2022-09-09 09:38:26 +0100

    updates

--- Data Structures and Algorithms/Python/queue_1.py
+++ Data Structures and Algorithms/Python/queue_1.py
@@ -109,6 +109,64 @@ class MyQueue:
         self._shift_stacks()
         return self.s1.top()
 
+# implementation of a LinkedQueue
+class LinkedQueue:
+    """Implementation of FIFO ADT using a singly linked-list for underlying storage"""
+    class _Node:
+        """Non-public utility for storing a node"""
+        __slots__ = '_element', '_next'
+
+        def __init__(self, element, next):
+            self._element = element
+            self._next = next
+
+    #----------------public methods-----------------------------------------
+    def __init__(self):
+        """Initialize a queue"""
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def is_empty(self):
+        return self._size == 0
+
+    def __len__(self):
+        return self._size
+
+    def first(self):
+        """Return but do not remove the element at the front of the queue
+        
+        Raises Empty exception if the Queue is empty
+        """
+        if self.is_empty():
+            raise Empty("The Queue is empty.")
+        return self._head._element
+
+    def enqueue(self, e):
+        """Add an element to the back of the queue"""
+        newest = self._Node(e, None) # create the node
+        if self.is_empty():
+            self._head = newest
+        else:
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+    def dequeue(self):
+        """Return and remove an element from the front of the queue
+        
+        Raises Empty exception if the Queue is empty
+        """
+        if self.is_empty():
+            raise Empty("The Queue is empty.")
+        ans = self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        if self.is_empty():
+            self._tail = None
+        return ans
+
+
 
 if __name__ == '__main__':
     q = ArrayQueue()
@@ -131,4 +189,17 @@ if __name__ == '__main__':
     print(len(q))
     print(q.dequeue())
     print(q.is_empty())
+    print(len(q))
+
+    
+    print('\nQueue')
+    q = LinkedQueue()
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    q.enqueue(4)
+    print(len(q))
+    q.dequeue()
+    print(q.is_empty())
+    print(q.first())
     print(len(q))
--- Data Structures and Algorithms/Python/stack.py
+++ Data Structures and Algorithms/Python/stack.py
@@ -42,6 +42,55 @@ class ArrayStack:
         """Returns a string representation of the current stack in memory"""
         return f"Stack: {self._data}"
 
+class LinkedStack:
+    """A LIFO ADT implementation using a singly-linked list as underlying storage"""
+
+#--------------non-public utility-------------------------------------------
+    class _Node:
+        """Non-public class for storing a node"""
+        __slots__ = '_element', '_next'
+        def __init__(self, element, next):
+            self._element = element
+            self._next = next
+
+#--------------------stack methods------------------------------------------------
+    def __init__(self):
+        """Initializes the stack"""
+        self._head = None
+        self._size = 0
+
+    def is_empty(self):
+        return self._size == 0
+
+    def __len__(self):
+        return self._size
+
+    def push(self, e):
+        """Add an element e to the top of the stack"""
+        self._head = self._Node(e, self._head)
+        self._size += 1
+
+    def pop(self):
+        """Return and remove an element from the top of the stack. 
+        
+        Returns Empty exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty("The stack is empty.")
+        ans = self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        return ans
+
+    def top(self):
+        """Return (but do not remove) an element from the top of the stack.
+        
+        Returns Empty exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty("The stack is empty.")
+        return self._head._element
+
 if __name__ == '__main__':
     s = ArrayStack()
     print(s.is_empty())
@@ -51,3 +100,11 @@ if __name__ == '__main__':
     print(s.is_empty())
     print(len(s))
     print(str(s))
+
+    s = LinkedStack()
+    print(s.is_empty())
+    s.push(2)
+    s.push(3)
+    print(s.top())
+    print(s.is_empty())
+    print(len(s))
