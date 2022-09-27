e16629118e80a6e587537fbdb13b6d53b19fa2fe
Akorede <akoredeadewole8@gmail.com>
2022-09-27 09:47:02 +0100

    update

--- Leetcode Challenges/Python/squares_sorted_array.py
+++ Leetcode Challenges/Python/squares_sorted_array.py
@@ -5,6 +5,7 @@ https://leetcode.com/problems/squares-of-a-sorted-array/
 """
 
 
+import collections
 from typing import List
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
@@ -14,3 +15,28 @@ class Solution:
         
         ans = sorted(result)
         return ans
+
+# approach 1 but better
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        arr = [v**2 for v in nums]
+        arr.sort()
+        return arr
+
+# using a deque
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = collections.deque(nums)
+        reverse = []
+
+        while ans:
+            left = ans[0] ** 2
+            right = ans[-1] ** 2
+
+            if left > right:
+                reverse.append(left)
+                ans.popleft()
+            else:
+                reverse.append(right)
+                ans.pop()
+        return reverse[::-1]
