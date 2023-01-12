485b8ec041f9b7440c54f90218c47dfdd431f260
Akorede <akoredeadewole8@gmail.com>
2023-01-12 11:33:29 +0100

    added update Jan 12

--- Leetcode Challenges/Python/pascals_triangle.py
+++ Leetcode Challenges/Python/pascals_triangle.py
@@ -2,4 +2,20 @@
 Solution to Leetcode problem #118 - Pascal's Triangle
 
 https://leetcode.com/problems/pascals-triangle/
+
 """
+class Solution:
+    def generate(self, numRows):
+        results = [[1], [1, 1]] # row 0 and row 1
+
+        # start filling from row 2
+        for i in range(2, numRows):
+            row = [1]
+
+            for j in range(1, len(results[i - 1])): # needs to generate this amount of numbers
+                ans = results[i - 1][j] + results[i - 1][j - 1]
+                row.append(ans)
+                row.append(1)
+
+                results.append(row)
+        return results[::numRows]
--- mini projects/Billboard/billboard.py
+++ mini projects/Billboard/billboard.py
@@ -10,7 +10,7 @@ import sys
 import os
 from datetime import date
 
-save_path = os.getcwd() + "\Billboard"
+save_path = os.getcwd() + "\mini projects\Billboard"
 today = date.today().strftime("%d %b, %Y")
 
 def query_user():
