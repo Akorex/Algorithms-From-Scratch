26f90ad2e769ab11a89378004016b9f0d38b0133
Akorede <akoredeadewole8@gmail.com>
2023-02-02 14:30:46 +0100

    added verify alien's dictionary

---
+++ Leetcode Challenges/Python/verify_alien.py
@@ -0,0 +1,31 @@
+"""
+Solution to Leetcode problem # 953 - Verifying an alien dictionary
+
+https://leetcode.com/problems/verifying-an-alien-dictionary/
+"""
+
+# initial approach - does not work with repeated characters in the word
+# & doesn't increase the index of characters seen
+from typing import List
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        index = 0
+        for word in words:
+            for c in word:
+                if word.index(c) != order[index]:
+                    return False
+        return True
+
+# let's improve on it by using a hash map to store the counts of characters in a dictionary
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        ordered = {ch: i for i, ch in enumerate(order)}
+        prev = list(ordered[ch] for ch in words[0])
+
+        for i in range(1, len(words)):
+            curr = list(ordered[ch] for ch in words[i])
+            if curr < prev:
+                return False
+            prev = curr
+        return True
