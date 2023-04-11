cf64883eb5c27d96c9e97ad079ae79bf3df42667
Akorede <akoredeadewole8@gmail.com>
2023-04-11 10:01:47 +0100

    added selection sort

--- Data Structures and Algorithms/Python/sorting_algorithms.py
+++ Data Structures and Algorithms/Python/sorting_algorithms.py
@@ -14,3 +14,26 @@ def insertion_sort(A):
             A[j] = A[j - 1]
             j -= 1
         A[j] = curr
+
+    return A
+
+def selection_sort(A):
+    """Sort list of comparable elements into non-decreasing order"""
+    for i in range(len(A)):
+        min_index = i
+
+        for j in range(i + 1, len(A)):
+            if A[j] < A[min_index]:
+                min_index = j
+
+        # Swap the minimum element with the first element of the unsorted part
+        A[i], A[min_index] = A[min_index], A[i]
+    
+    return A
+
+
+if __name__ == '__main__':
+    l = [2, 3, 4, 1, 0]
+
+    print(insertion_sort(l))
+    print(selection_sort(l))
