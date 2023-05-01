1ef4ea6f92db7bf949e20d641bea9515c1147981
Akorede Adewole <akoredeadewole8@gmail.com>
2023-05-01 17:55:17 +0100

    added merge sort

---
+++ Data Structures and Algorithms/Python/merge_sort.py
@@ -0,0 +1,47 @@
+"""An implementation of merge sort algorithm in python"""
+
+def merge_sort(S):
+    """Returns a sorted array S using the merge - sort algorithm"""
+    
+    # divide the arrays
+    if len(S) < 2:
+        return
+    
+    mid = len(S)//2
+    left = S[:mid]
+    right = S[mid:]
+
+    # recursively divide the arrays
+    merge_sort(left)
+    merge_sort(right)
+
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            S[k] = left[i]
+            i += 1
+        else:
+            S[k] = right[j]
+            j += 1
+        
+        k += 1
+
+    # copy the remaining elements to S
+    while i < len(left):
+        S[k] = left[i]
+        i += 1
+        k += 1
+    
+    while j < len(right):
+        S[k] = right[j]
+        j += 1
+        k += 1
+
+    return S
+
+
+if __name__ == '__main__':
+    l = [2, 3, 4, 1, 0]
+
+    print(merge_sort(l))
