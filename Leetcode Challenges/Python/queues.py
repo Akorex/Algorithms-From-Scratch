1c1f124cee17159bb549a32bc7661da85e69beec
Akorede <akoredeadewole8@gmail.com>
2024-09-16 20:51:36 +0100

    added circular queue

--- Leetcode Challenges/Python/queues.py
+++ Leetcode Challenges/Python/queues.py
@@ -97,6 +97,56 @@ class LinkedQueue:
         self._tail = newest
         self._size += 1
 
+class CircularQueue:
+    """Queue implementation using a circularly linked list for storage"""
+    class _Node:
+        __slots__ = '_element', '_next'
+
+        def __init__(self, element, next):
+            self._element = element
+            self._next = next
+
+    def __init__(self):
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+    
+    def is_empty(self):
+        return self._size == 0
+    
+    def first(self):
+        if self.is_empty():
+            raise Empty("The Queue is empty")
+        head = self._tail._next
+        return head._element
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise Empty("The Queue is empty")
+        head = self._tail._next
+        e = head._element
+
+        if self._size == 1:
+            self._tail = None
+        else:
+            self._tail._next = head._next
+        self._size -= 1
+        return e
+    
+    def enqueue(self, e):
+        newest = self._Node(e, None)
+
+        if self.is_empty():
+            newest._next = newest
+        else:
+            newest._next = self._tail._next
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
+
+
 
 
 
