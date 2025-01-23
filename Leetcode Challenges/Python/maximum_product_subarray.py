b0fc23165c41e440b9a2d66f06601aa4ee64e5e3
Akorede Adewole <43486426+Akorex@users.noreply.github.com>
2025-01-23 12:10:27 +0100

    Leetcode: maximum_subproduct_array

---
+++ Leetcode Challenges/Python/maximum_product_subarray.py
@@ -0,0 +1,46 @@
+"""
+Solution to Leetcode problem #152 - Maximum Product Subarray
+
+https://leetcode.com/problems/maximum-product-subarray/
+"""
+from typing import List
+
+# initial approach, doesn't pass all cases
+# wrong because of the following:
+# Incorrect initialization of currMax which is wrong when all the numbers in an array are negative
+# not considering negative elements
+# not considering single elements
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        currMax = 0
+
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        for i in range(1, len(nums)):
+            tempMax = nums[i-1] * nums[i]
+
+            if tempMax > currMax:
+                currMax = tempMax
+        return currMax
+    
+
+# correct approach
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        prev_max = nums[0]
+        prev_min = nums[0]
+        ans = nums[0]
+
+        for i in range(1, len(nums)):
+            curr_max = max(nums[i], nums[i] * prev_max, nums[i] * prev_min)
+            curr_min = min(nums[i], nums[i] * prev_max, nums[i] * prev_min)
+
+            prev_max = curr_max
+            prev_min = curr_min
+
+            ans = max(ans, curr_max)
+
+        return ans
