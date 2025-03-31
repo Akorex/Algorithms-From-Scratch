a2a78d0426cfa3d09c947e1a676c34edea78ae4f
Akorede Adewole <43486426+Akorex@users.noreply.github.com>
2025-03-31 16:04:00 +0100

    "Add two numbers"

--- Leetcode Challenges/Python/old/Add_two_numbers.py
+++ Leetcode Challenges/Python/old/Add_two_numbers.py
@@ -1,3 +1,10 @@
+"""
+Solution to Leetcode problem #2 - Add two numbers
+
+
+https://leetcode.com/problems/add-two-numbers/
+"""
+
 class ListNode:
     def __init__(self, val = 0, next = None):
         self.val = val
@@ -24,3 +31,26 @@ def addTwoNumbers(l1, l2):
         l1 = l1.next if l1 else None
         l2 = l2.next if l2 else None
     return result.next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        ans = ListNode(0)
+        curr = ans
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry != 0:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            col_sum = l1_val + l2_val + carry
+
+            carry, rem = divmod(col_sum, 10)
+
+            remNode = ListNode(rem)
+            curr.next = remNode
+            curr = remNode
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return ans.next 
